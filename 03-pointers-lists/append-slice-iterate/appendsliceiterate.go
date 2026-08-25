dddf9156fd66268b963b3e15e1babd2c393e254c
Raskolnikov <jerry.tann@endress.com>
2026-08-25 11:47:39 +0800

    📝 more go

---
+++ 03-pointers-lists/append-slice-iterate/appendsliceiterate.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s []int
+	printSlice(s)
+
+	// append works on nil slices.
+	s = append(s, 0)
+	printSlice(s)
+
+	// The slice grows as needed.
+	s = append(s, 1)
+	printSlice(s)
+
+	// We can add more than one element at a time.
+	s = append(s, 2, 3, 4)
+	s2 := make([]int, len(s))
+	for i, v := range s {
+		s2[i] = v * 2
+	}
+	printSlice(s2)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
--- 03-pointers-lists/slice/slice.go
+++ 03-pointers-lists/slice/slice.go
@@ -5,6 +5,11 @@ import "fmt"
 func main() {
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 
-	var s []int = primes[1:4]
+	var s []int = primes[1:4] // notice no size
+	fmt.Printf("len is %d", len(s))
 	fmt.Println(s)
 }
+
+// arrays actually rare in go code.
+// they are like freeze in ruby
+// "zero" value of slice is nil
